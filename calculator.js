bfa78e4083b20d8733a8aca39bbe83a6f16ae211
Sakibul Islam <120340281+SI2105@users.noreply.github.com>
2024-01-21 20:39:50 +0000

    operate function added
    operate function takes operator and two numbers to carry out arithmetic operator.

--- calculator.js
+++ calculator.js
@@ -13,6 +13,27 @@ const add = function(a,b) {
   const divide = function(a,b){
     return a/b;
   };
+
+  const operate = function(num1,operator,num2){
+    if (operator === "+"){
+        return add(num1,num2)
+    }
+    else if (operator === "-"){
+        return subtract(num1,num2);
+    }
+
+    else if(operator === "*"){
+        return multiply(num1,num2);
+    }
+
+    else if(operator === "/"){
+        return divide(num1,num2)
+    }
+  }
   
 
-  
+  let number1 = 0;
+  let operator = null;
+  let number2 = 0;
+
+
